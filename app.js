12ae9549ef69aa8f24892de08aa7a5a64f4ebfaa
ayushx4 <ayushvadodariya12@gamil.com>
2025-01-08 11:43:16 +0530

    v0.0.1

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 import ollama from "ollama";
+import fs from "fs";
 
-(async ()=>{
+function readFile(filePath){
+    return new Promise((resolve, reject)=>{
+      fs.readFile(filePath,(err,data)=>{
+        if(err) reject(err.message);
+        resolve(data.toString());
+      })
+    })
+}
+
+function createAndWriteFile(filePath, content){
+  return new Promise((resolve, reject)=>{
+    fs.writeFileSync(filePath,content,(err)=>{
+      if(err) reject(err.message);
+      resolve();
+    });
+  })
+}
+
+async function askToOllama(content){
   try{
     const response = await ollama.chat({
-      model: 'moondream:latest',
-      messages: [{ role: 'user', content: 'Why is the sky blue?' }],
+      model: 'llama3.2',
+      messages: [{ role: 'user',content:content}],
     })
-    console.log(response.message.content)
+    return response;
   }catch(error){
     console.error("Error occurred:", error.message);
   }
-})()
+}
 
+ async function main(){
+  const content =  await readFile("./questions/q.txt");
+  const ollamaResponse = await askToOllama(content);
+  await createAndWriteFile("answers/a.txt", ollamaResponse.message.content);
+}
+main();
